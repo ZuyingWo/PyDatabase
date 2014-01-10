23f276eb130e2a71b1616fc432ba9a9b51fdcf05
williewonka <frankieboy03@gmail.com>
2014-01-10 11:27:32 +0100

    added escape and insert method, made condition more resilient

--- example.py
+++ example.py
@@ -7,4 +7,6 @@ print(db.Update(table='test', values={'testtext': 'test'}, condition_and={'ID': 
 print(db.query)
 print(db.Select(table="test", columns=["testtext", "testinteger"], condition_and={'ID': 1}))
 print(db.query)
+print(db.Insert(table='test', values={'testtext': 'een insert', 'testinteger' : '76'}))
+print(db.query)
 db.Close()
