4cf13ac797909b6051d1985a181d962a5a42c1b2
williewonka <frankieboy03@gmail.com>
2014-01-15 21:49:21 +0100

    update proper escape method

--- example.py
+++ example.py
@@ -2,11 +2,11 @@ __author__ = 'williewonka'
 
 import PyDatabase
 
-db = PyDatabase.PyDatabase()
-print(db.Update(table='test', values={'testtext': 'test'}, condition_and={'ID': 1}))
+db = PyDatabase.PyDatabase(host="localhost",user="root",passwd="",db="test")
+print(db.Update(table='test', values={'testtext': 'test2'}, condition_and={'ID': 1}))
 print(db.query)
 print(db.Select(table="test", columns=["testtext", "testinteger"], condition_and={'ID': 1}))
 print(db.query)
-print(db.Insert(table='test', values={'testtext': 'een insert', 'testinteger' : '76'}))
+print(db.Insert(table='test', values={'testtext': "new 'foo' insert", 'testinteger' : '76'}))
 print(db.query)
 db.Close()
